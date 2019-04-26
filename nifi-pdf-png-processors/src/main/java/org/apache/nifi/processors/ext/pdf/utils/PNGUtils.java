5b023760530ab82bae527803740c12329bd6ba37
coco11563 <coco11563@yeah.net>
2019-04-26 18:39:12 +0800

    准备写几个特性

--- nifi-pdf-png-processors/src/main/java/org/apache/nifi/processors/ext/pdf/utils/PNGUtils.java
+++ nifi-pdf-png-processors/src/main/java/org/apache/nifi/processors/ext/pdf/utils/PNGUtils.java
@@ -1,5 +1,7 @@
 package org.apache.nifi.processors.ext.pdf.utils;
 
+import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.processor.ProcessSession;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.PDFRenderer;
 import org.slf4j.Logger;
@@ -8,6 +10,9 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Vector;
 
 public class PNGUtils {
@@ -56,6 +61,80 @@ public class PNGUtils {
         }
     }
 
+    public static List<FlowFile> pdf2Image(File PdfFilePath,
+                                           int dpi,
+                                           Logger logger,
+                                           ProcessSession session,
+                                           Vector<String> failAccumulator) {
+        PDDocument pdDocument;
+        List<FlowFile> ret = new LinkedList<>();
+        try {
+            int dot = PdfFilePath.getName().lastIndexOf('.');
+            String imagePDFName = PdfFilePath.getName().substring(0, dot); // 获取图片文件名
+            String imgFilePathPrefix = imagePDFName.substring(0, 2);
+            pdDocument = PDDocument.load(PdfFilePath);
+            PDFRenderer renderer = new PDFRenderer(pdDocument);
+            int pages =  pdDocument.getNumberOfPages();
+            for (int i = 0; i < pages; i++) {
+                BufferedImage image = renderer.renderImageWithDPI(i, dpi);
+                FlowFile ff = session.create();
+                ff = session.write(ff, out -> {
+                    ImageIO.write(image, "png", out);
+                });
+                ff = session.putAttribute(ff, "index", "" + (i + 1)); //index
+                ff = session.putAttribute(ff, "parent", PdfFilePath.getName()); //origin name
+                ff = session.putAttribute(ff, "name", imagePDFName + "_" + (i + 1) + ".png");
+                ff = session.putAttribute(ff, "prefix", imgFilePathPrefix);
+                ret.add(ff);
+            }
+            logger.info("PDF文档\"" + PdfFilePath + "\"转PNG图片成功！");
+        } catch (IOException e) {
+            failAccumulator.add(PdfFilePath.getName());
+            logger.error("无法转换的pdf路径为：" + PdfFilePath + "原因：" + e.getMessage());
+        }
+        return ret;
+    }
+
+    public static List<FlowFile> pdf2Image(FlowFile pdfff,
+                                           int dpi,
+                                           Logger logger,
+                                           ProcessSession session,
+                                           Vector<FlowFile> failAccumulator) {
+        PDDocument pdDocument;
+        List<FlowFile> ret = new LinkedList<>();
+        String fileName = null;
+        try {
+            fileName = pdfff.getAttribute("filename");
+            int dot = fileName.lastIndexOf('.');
+            String imagePDFName = fileName.substring(0, dot); // 获取图片文件名
+            String imgFilePathPrefix = fileName.substring(0, 2);
+            pdDocument = PDDocument.load(session.read(pdfff));
+            PDFRenderer renderer = new PDFRenderer(pdDocument);
+            int pages =  pdDocument.getNumberOfPages();
+            for (int i = 0; i < pages; i++) {
+                BufferedImage image = renderer.renderImageWithDPI(i, dpi);
+                FlowFile ff = session.create();
+                ff = session.write(ff, out -> {
+                    ImageIO.write(image, "png", out);
+                });
+                ff = session.putAttribute(ff, "index", "" + (i + 1)); //index
+                ff = session.putAttribute(ff, "parent", fileName); //origin name
+                ff = session.putAttribute(ff, "name", imagePDFName + "_" + (i + 1) + ".png");
+                ff = session.putAttribute(ff, "prefix", imgFilePathPrefix);
+                ret.add(ff);
+            }
+            logger.info("PDF文档\"" + fileName + "\"转PNG图片成功！");
+        } catch (IOException e) {
+            if (fileName == null) {
+                logger.error("上游的FlowFile文件不含filename属性");
+                failAccumulator.add(pdfff);
+            } else {
+                failAccumulator.add(pdfff);
+                logger.error("无法转换的pdf路径为：" + fileName + "原因：" + e.getMessage());
+            }
+        }
+        return ret;
+    }
     private static boolean createDirectory(String folder) {
         File dir = new File(folder);
         if (dir.exists()) {
